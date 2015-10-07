32f5b224e488b9311dc12f397f8d8ca372a0dd6e
Livanux <ivangastelum.tf@gmail.com>
2015-10-07 15:45:01 -0700

    Update

--- js/mainviews.js
+++ js/mainviews.js
@@ -1,23 +1,65 @@
+ $(document).ready(function() {
+        
+       $("#content").load("views/home.html");
+
+});
+
 function loadhome(){
 	$("#content").load("views/home.html");
+	$('#1').attr("class", "active"); 
+	$('#2').attr("class", ""); 
+	$('#3').attr("class", ""); 
+	$('#4').attr("class", ""); 
+	$('#5').attr("class", ""); 
+	$('#6').attr("class", "");
 }
 
 function loadlista(){
 	$("#content").load("views/lista_espera.html");
+	$('#1').attr("class", ""); 
+	$('#2').attr("class", "active"); 
+	$('#3').attr("class", ""); 
+	$('#4').attr("class", ""); 
+	$('#5').attr("class", ""); 
+	$('#6').attr("class", "");
 }
 
 function loadalta(){
 	$("#content").load("views/alta_espera.html");
+	$('#1').attr("class", ""); 
+	$('#2').attr("class", "");
+	$('#3').attr("class", "active"); 
+	$('#4').attr("class", "");
+	$('#5').attr("class", "");
+	$('#6').attr("class", "");
 }
 
 function loadestadisticas(){
 	$("#content").load("views/estadisticas.html");
+	$('#1').attr("class", "");
+	$('#2').attr("class", "");
+	$('#3').attr("class", ""); 
+	$('#4').attr("class", "active");
+	$('#5').attr("class", ""); 
+	$('#6').attr("class", "");
 }
 
 function loadalumnos(){
 	$("#content").load("views/alumnos.html");
+	$('#1').attr("class", "");
+	$('#2').attr("class", "");
+	$('#3').attr("class", ""); 
+	$('#4').attr("class", "");
+	$('#5').attr("class", "active");
+	$('#6').attr("class", "");
 }
 
 function loadguia(){
 	$("#content").load("views/guia.html");
+	$('#1').attr("class", "");
+	$('#2').attr("class", "");
+	$('#3').attr("class", ""); 
+	$('#4').attr("class", "");
+	$('#5').attr("class", "");
+	$('#6').attr("class", "active"); 
 }
