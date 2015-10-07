0642a009dc34e82a32c1311bb051e8b3cddb416f
Livanux <ivangastelum.tf@gmail.com>
2015-10-07 01:14:52 -0700

    Views created

---
+++ js/mainviews.js
@@ -0,0 +1,23 @@
+function loadhome(){
+	$("#content").load("views/home.html");
+}
+
+function loadlista(){
+	$("#content").load("views/lista_espera.html");
+}
+
+function loadalta(){
+	$("#content").load("views/alta_espera.html");
+}
+
+function loadestadisticas(){
+	$("#content").load("views/estadisticas.html");
+}
+
+function loadalumnos(){
+	$("#content").load("views/alumnos.html");
+}
+
+function loadguia(){
+	$("#content").load("views/guia.html");
+}
