9f662ebfb1f76a2a03e6fd4b36f577cf918491fb
David Guillen Vazquez <guillenvd@gmail.com>
2015-10-30 01:35:22 -0700

    Lista y total alumnos

--- php/altaAlumnos.php
+++ php/altaAlumnos.php
@@ -1,40 +1,70 @@
 <?php
+/*
+	Include principal para la conexión a la base de datos
+*/
 include("conexion.php");
-$FullName ="dav ";
-$Date = date("Y-m-d H:m:s");
-$sqlCoutn = "SELECT COUNT(turno) as atendidos FROM alumnos where estado = 2";
-$sqlGetprom = "SELECT * FROM alumnos where estado = 2";
-$result = $conn->query($sqlCoutn);
-$promedioTiempo=0;
+
+/*
+	Definición de las variables que se envian mediante la función get
+	Las variables 
+	@Nombre [Es el nombre completo concatenado  string]
+	@Ficha 	[Es la ficha de inscripción del aspirant a alumno Int]
+	@Carrera [El identificador de la carrera seleccionada  Int]
+*/
+$FullName = $_GET["Nombre"];
+$Ficha = $_GET["Ficha"];
+$Carrera = $_GET["Carrera"];
+
+
+$Date = date("Y-m-d H:m:s");// Fecha actual, ejemplo 2015-12-31 23:59:59
+$Indice = date("Y-m-d");// Fecha actual, ejemplo 2015-12-31 23:59:59
+$promedioTiempo=0; //Declaración de la variable del tiempo estimado
+
+//Scrips sql
+$sqlCoutn = "SELECT COUNT(turno) as atendidos FROM alumnos where estado = 2 and indice ='".$Indice."'"; //Query para contar a los alumnos atendidos
+$sqlGetprom = "SELECT * FROM alumnos where estado = 2 and indice ='".$Indice."'"; //Query para selecionar a todos los alumnos atendidos
+$sqlGetTurno = "SELECT count(id) as turno FROM alumnos WHERE  indice ='".$Indice."'";
+// ir por cantidad de alumnos
+	$result = $conn->query($sqlGetTurno); //ir por los aspirantes del día en curso
+	    while($row = $result->fetch_assoc()) {
+	    	$turnos = $row["turno"] + 1;
+	    }
+	
+
+// Verificar el tiempo promedio para el alumno
+	$result = $conn->query($sqlCoutn); //Ejecutar Query $sqlCoutn
     while($row = $result->fetch_assoc()) {
+    	// si existen como minimo dos alumnos atendidos hoy
         if ($row["atendidos"] >= 2){
+        	//si hay más de dos se genera una suma de los minutos que tardaron en ser atendidos
+        	// mediante el query de seleccion todos lo atnedidos hoy
         	$sumaProm =0;
-        	$result = $conn->query($sqlGetprom);
-		    while($rowAtendidos = $result->fetch_assoc()){
+        	$resultProm = $conn->query($sqlGetprom);
+		    while($rowAtendidos = $resultProm->fetch_assoc()){
 		    	$time1 =  new DateTime($rowAtendidos['hora_inicio']);
 		    	$time2 =  new DateTime($rowAtendidos['hora_fin']);
-		        $suma = $time1->diff($time2);
-		        $sumaProm += (int)$suma->format('%i');
+		        $suma = $time1->diff($time2); // tiempo que tardó en ser atendido x alumno
+		        $sumaProm += (int)$suma->format('%i'); //Suma de los alumnos
 		    }
-		    $promedioTiempo=ceil( $sumaProm  / $result->num_rows) ;
+		    $promedioTiempo=ceil( $sumaProm  / $resultProm->num_rows) ; //promedio de todos los tiempos, se redondea a su entero más proximo
         }
         else{
+        	//sino se asigna un tiempo default, en este caso 11 minutos
         	$promedioTiempo = 11;
         }
     }
 
-echo  $promedioTiempo;
-/*
-	contar numero de alumnos
-	cual promedio de tiempo
-	$sql = "INSERT INTO alumnos (nombre, fecha, tiempo_estimado, turno, estado, carrera,ficha_inscripcion)
-	VALUES ('".$FullName ."','".$Date."', '00:15:00',1,1,1,1)";
+	$sql = "INSERT INTO alumnos (nombre,turno,hora_inicio,estado,carrera,ficha_inscripcion,indice) 
+	VALUES ('".$FullName."',".$turnos.",'".$Date."',1,".$Carrera.",".$Ficha.",'".$Indice."' );";
 	if ($conn->query($sql) === TRUE) {
-	} else {
+		$bandera =1;
+	} 
+	else {
+		$bandera = 0;
 	}
 	$conn->close();
 
-*/
-	$json = array("key1" => $_GET["Nomb"],"key2" => $_GET["Fi"],"key3" => $_GET["Carrera"]);
+
+	$json = array("key1" => $bandera,"tiempo" =>$promedioTiempo,"turno" =>$turnos );
 	echo json_encode($json);
 ?>
