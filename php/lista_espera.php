4cb591cbbf4a6268f592d52da5c1181774dec504
guillenvd <guillenvd@gmail.com>
2015-10-31 21:19:54 -0700

    Update Php

--- php/lista_espera.php
+++ php/lista_espera.php
@@ -4,11 +4,11 @@ include("conexion.php");
 $Indice = date("Y-m-d");// Fecha actual, ejemplo 2015-12-31 23:59:59
 $sqlGetLista = "SELECT nombre, turno, estado, carrera, ficha_inscripcion FROM alumnos where indice ='".$Indice."'";
 $return_arr = array();
-
+$array = array();
 $result = $conn->query($sqlGetLista); 
-
 	while($row = mysqli_fetch_array($result)) {
-	    array_push($return_arr,$row);
+		$array = array('Nombre' =>$row['nombre'],'Turno' =>$row['turno'],'Estado' =>$row['estado'],'Carrera' =>$row['carrera'],'Ficha' =>$row['ficha_inscripcion']);
+	    array_push($return_arr,$array);
 	}
 
 	$array = json_encode($return_arr);
---
+++ php/updateStatus.php
@@ -0,0 +1,18 @@
+<?php
+/*	Include principal para la conexión a la base de datos*/
+include("conexion.php");
+$Indice = date("Y-m-d");// Fecha actual, ejemplo 2015-12-31 23:59:59
+$return_arr = array();
+
+$sqlUpdate = "UPDATE alumnos SET estado=".(int)$_GET['Estado']." WHERE ficha_inscripcion=".(int)$_GET['Ficha']." and indice='".$Indice."'";
+
+if ($conn->query($sqlUpdate) === TRUE) {
+   	$return_arr = array('check' =>1);
+
+} else {
+	$return_arr = array('check' =>0);
+}
+	$array = json_encode($return_arr);
+	echo $array
+
+?>
